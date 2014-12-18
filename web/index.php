242deedd2135661612e60a4b5cb4c87ff0014e3a
AlejandroHerr <alejandrohnc88@gmail.com>
2014-12-19 00:27:07 +0100

    Silex bootstrap

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
-//Set right timezone
-define('ROOT',dirname(dirname(__FILE__)));
+
+define('ROOT', dirname(dirname(__FILE__)));
 date_default_timezone_set('Europe/Madrid');
 
-$app = require ROOT.'/src/app.php';
+$app = include ROOT.'/src/app.php';
 
 $app->run();
