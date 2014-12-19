1238cf4af857e02ca926fab1b19231c849f2e29e
AlejandroHerr <alejandrohnc88@gmail.com>
2014-12-19 07:32:12 +0100

    Autogenerated file

---
+++ src/Esnuab/Libro/Model/SocioQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Esnuab\Libro\Model;
+
+use Esnuab\Libro\Model\Base\SocioQuery as BaseSocioQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'socio' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SocioQuery extends BaseSocioQuery
+{
+
+}
