55f43a3ee0f79de652a7702847e1c2859c7c558b
AlejandroHerr <alejandrohnc88@gmail.com>
2014-12-19 07:31:32 +0100

    Refactored

--- src/Lib/AlejandroHerr/Controller/RestControllerInterface.php
+++ src/Lib/AlejandroHerr/Controller/RestControllerInterface.php
@@ -1,16 +1,9 @@
 <?php
 namespace AlejandroHerr\Controller;
 
-use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 
-abstract class AbstractApiController
+interface RestControllerInterface
 {
-    public function payloadToJson(Application $app, Request $request)
-    {
-        if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-            $data = json_decode($request->getContent(), true);
-            $request->request->replace(is_array($data) ? $data : array());
-        }
-    }
+    public function parsePayload(Request $request);
 }
