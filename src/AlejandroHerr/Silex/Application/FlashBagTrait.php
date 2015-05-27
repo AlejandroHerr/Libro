90574cb4eca5e4932e262b8126f19ac51853ccd2
AlejandroHerr <alejandrohnc88@gmail.com>
2015-05-27 17:03:56 +0200

    FlashBag Trait

---
+++ src/AlejandroHerr/Silex/Application/FlashBagTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AlejandroHerr\Silex\Application;
+
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+
+trait FlashBagTrait
+{
+    /**
+     * Adds a flash message for type.
+     *
+     * @param string $type
+     * @param string $message
+     *
+     * @see FlashBag::add($type, $message)
+     */
+    public function addFlashBag($type, $message)
+    {
+        $this['session']->getFlashBag()->add($type, $message);
+    }
+
+    /**
+     * Gets all flash messages.
+     *
+     * @return array
+     *
+     * @see FlashBag::all()
+     */
+    public function getFlashBag()
+    {
+        return $this['session']->getFlashBag()->all();
+    }
+}
