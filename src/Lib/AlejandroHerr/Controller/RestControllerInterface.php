cee5fda73bffcd748e257a8832969b9c1195af3a
AlejandroHerr <alejandrohnc88@gmail.com>
2015-05-27 17:06:16 +0200

    New folder structure

--- src/Lib/AlejandroHerr/Controller/RestControllerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace AlejandroHerr\Controller;
-
-use Symfony\Component\HttpFoundation\Request;
-
-interface RestControllerInterface
-{
-    public function parsePayload(Request $request);
-}
--- src/Lib/AlejandroHerr/Exception/Model/ModelExceptionInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace AlejandroHerr\Exception\Model;
-
-interface ModelExceptionInterface
-{
-}
