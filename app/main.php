a3a03b79b3130f84eba5e81bf062d840bc211e3d
AlejandroHerr <alejandrohnc88@gmail.com>
2015-05-28 18:04:41 +0200

    New main app

---
+++ app/main.php
@@ -0,0 +1,57 @@
+<?php
+
+use AlejandroHerr\Silex\Application;
+use AlejandroHerr\Silex\SlashControllerResolver;
+use EsnUab\Libro\Controller\MainControllerProvider;
+use Herrera\Wise\WiseServiceProvider;
+use Propel\Silex\PropelServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use AlejandroHerr\Silex\EventListener\TemplateRenderingListener;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use EsnUab\Twig\UtilsExtensionProvider;
+use EsnUab\Libro\Views\LibroTemplatesServiceProvider as LibroTemplates;
+use EsnUab\Libro\EventListener\SocioListener;
+use EsnUab\Twig\TwBsExtension\TwBsExtensionProvider;
+
+$app = new Application();
+
+$app['debug'] = true;
+$app['locale'] = 'es';
+$app['route_class'] = 'AlejandroHerr\\Silex\\Route';
+$app['dispatcher']->addSubscriber(new TemplateRenderingListener($app));
+$app['dispatcher']->addSubscriber(new SocioListener($app));
+$app['resolver'] = $app->share($app->extend('resolver', function ($resolver, $app) {
+    return new SlashControllerResolver('libro', $app, $app['logger']);
+}));
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => ROOT.'/var/development.'.date('Ymd').'.log',
+));
+$app->register(new WiseServiceProvider(), ['wise.path' => ROOT.'/config']);
+$app->register(new PropelServiceProvider(), ['propel.config_file' => ROOT.'/config/config.php']);
+$app->register(new UrlGeneratorServiceProvider());
+
+$app->register(new FormServiceProvider(), ['form.secret' => md5('random')]);
+$app->register(new SessionServiceProvider());
+$app->register(new TranslationServiceProvider(), ['locale' => ['es'], 'locale_fallbacks' => ['es']]);
+
+$app->register(new TwigServiceProvider(), [
+    'twig.options' => ['debug' => true],
+    'twig.form.templates' => ['bootstrap_3_horizontal_layout.html.twig'],
+]);
+$app->register(new LibroTemplates());
+$app->register(new UtilsExtensionProvider());
+$app->register(new TwBsExtensionProvider());
+$app['assets_path'] = $app->share(function () use ($app) {
+    $request = $app['request'];
+
+    return $request->getScheme().'://'.$request->getHost().'/public/assets';
+});
+
+$mainControllerProvider = new MainControllerProvider();
+$app->register($mainControllerProvider);
+$app->mount('/socios', $mainControllerProvider);
+
+$app->run();
--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
 <?php
+
+//GLOBALS
 define('ROOT', dirname(dirname(__FILE__)));
 date_default_timezone_set('Europe/Madrid');
 
-include ROOT.'/app/app.php';
+//AUTOLOAD
+$loader = require ROOT.'/vendor/autoload.php';
+
+//ERROR HANDLING
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+
+ErrorHandler::register();
+ExceptionHandler::register();
+
+//APP
+include ROOT.'/app/main.php';
