991e41c5f2e2d6ee9fb6cbb4645d238216a4d1ac
AlejandroHerr <alejandrohnc88@gmail.com>
2015-06-05 17:14:34 +0200

    Socio Crud Controller

---
+++ src/EsnUab/Libro/Controller/SocioController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace EsnUab\Libro\Controller;
+
+use EsnUab\Libro\Form\SocioType;
+use EsnUab\Libro\Model\Socio;
+use EsnUab\Libro\Model\SocioQuery;
+use EsnUab\Libro\Model\SocioVersion;
+use Silex\Application;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use EsnUab\Libro\EventListener\SocioEvents;
+use EsnUab\Libro\EventListener\Event\SocioEvent;
+
+class SocioController
+{
+    public function createAction(Application $app, Request $request)
+    {
+        $socio = new Socio();
+        $form = $this->getForm($app, $socio);
+
+        if ('POST' !== $request->getMethod()) {
+            return ['form' => $form->createView()];
+        }
+
+        if (!$this->processForm($request, $form, $socio)) {
+            $app->addFlashBag('danger', 'Hay problemas con los datos del socio.');
+
+            return ['form' => $form->createView()];
+        }
+
+        $app['dispatcher']->dispatch(SocioEvents::SOCIO_CREATED, new SocioEvent($socio));
+
+        return ['form' => $this->getForm($app)->createView()];
+    }
+    public function deleteAction(Application $app, Socio $socio)
+    {
+        $socio->setRemoved(true)
+            ->save();
+
+        return $app->redirect($app->path('socio.read', ['socio' => $socio->getId()], 'GET'));
+    }
+
+    public function readAction(Application $app, Socio $socio)
+    {
+        $response = $socio->toArray();
+        $response['versions'] = $socio->getAllVersionsInfo();
+
+        return ['socio' => $response];
+    }
+
+    public function readVersionAction(Application $app, Socio $socio, SocioVersion $version)
+    {
+        $response = $version->toArray();
+        $response['versions'] = $socio->getAllVersionsInfo();
+
+        return ['socio' => $response];
+    }
+
+    public function updateAction(Application $app, Request $request, Socio $socio)
+    {
+        $form = $app['form.factory']->createBuilder(new SocioType(), $socio, ['action_type' => 'editar'])->getForm();
+
+        if ('POST' !== $request->getMethod() || 'PUT' !== $request->getMethod()) {
+            return ['form' => $form->createView()];
+        }
+
+        if (!$this->processForm($request, $form, $socio)) {
+            $app->addFlashBag('danger', 'Hay problemas con los datos del socio.');
+
+            return ['form' => $form->createView()];
+        }
+
+        $app->addFlashBag('success', 'Datos actualizados correctamente.');
+
+        return $app->redirect($app->path('socio.read', ['socio' => $socio->getId()], 'GET'));
+    }
+
+    /**
+     * Middleware to find a Socio by its id.
+     *
+     * @param int $socio Id
+     *
+     * @return Socio Found Socio
+     *
+     * @throws HttpException If the Socio doesn't exist
+     */
+    public function findSocio($socio)
+    {
+        if (null === $socio = SocioQuery::create()->findPK($socio)) {
+            throw new HttpException(404, 'El socio no existe!');
+        }
+
+        return $socio;
+    }
+
+    public function findSocioVersion(Request $request)
+    {
+        $socio = $this->findSocio($request->attributes->get('socio'));
+        $version = $socio->getOneVersion($request->attributes->get('version'));
+
+        if (null == $version) {
+            $request->attributes->set('version', $socio);
+        } else {
+            $request->attributes->set('version', $version);
+        }
+
+        $request->attributes->set('socio', $socio);
+    }
+
+    /**
+     * Process the form of a create or update request.
+     *
+     * @param Request $request
+     * @param Form    $form
+     * @param Socio   $socio
+     *
+     * @return bool Returns true if the form was processed
+     */
+    protected function processForm(Request $request, Form $form, Socio $socio)
+    {
+        $form->handleRequest($request);
+
+        if ($socio->save()) {
+            return true;
+        }
+
+        foreach ($socio->getValidationFailures() as $failure) {
+            $form->get($failure->getPropertyPath())->addError(new FormError($failure->getMessage()));
+        }
+
+        return false;
+    }
+
+    public function queryAction(Application $app)
+    {
+        $pager = $app['request']->attributes->get('pager');
+        foreach ($pager as $socio) {
+            $socios[] = $socio->toArray();
+        }
+        $pagination = [
+            'page' => $pager->getPage(),
+            'first_page' => 1,
+            'last_page' => $pager->getLastPage(),
+            'limit' => $pager->getMaxPerPage(),
+            'sort_field' => substr($app['request']->attributes->get('sort'), 1),
+            'sort_sign' => substr($app['request']->attributes->get('sort'), 0, 1),
+        ];
+
+        $response = [
+            'socios' => $socios,
+            'pagination' => $pagination,
+        ];
+
+        return $response;
+    }
+
+    protected function getForm(Application $app, $socio = null, $action = 'crear')
+    {
+        $socio = $socio === null ? new Socio() : $socio;
+
+        return $app['form.factory']->createBuilder(new SocioType(), $socio, ['action_type' => $action])->getForm();
+    }
+}
