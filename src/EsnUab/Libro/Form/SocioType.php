dc95e934e3a195a696a0e4b8e71b345459859964
AlejandroHerr <alejandrohnc88@gmail.com>
2015-06-05 17:12:12 +0200

    Form

---
+++ src/EsnUab/Libro/Form/SocioType.php
@@ -0,0 +1,56 @@
+<?php
+namespace EsnUab\Libro\Form;
+
+use EsnUab\Libro\Model\Map\SocioTableMap;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SocioType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('esncard', 'text', [
+                'label' => 'ESNcard'
+            ])
+            ->add('nombre')
+            ->add('apellido')
+            ->add('dni')
+            ->add('email', 'email')
+            ->add('pais', 'country')
+            ->add('idioma', 'choice', [
+                'choices' => $this->getIdiomaValues()
+            ])
+            ->add('alta', 'date', [
+                'widget' => 'single_text',
+                'input' => 'string',
+                'format' => 'yyyy-MM-dd'
+            ]);
+
+        if ($options['action_type'] === 'crear') {
+            $builder->add('Crear', 'submit', ['attr' => ['class' => 'btn-success']]);
+        } elseif ($options['action_type'] === 'editar') {
+            $builder->add('version_comment', 'text', [
+                    'label' => 'Comentario'
+                ])
+                ->add('Guardar', 'submit', ['attr' => ['class' => 'btn-warning']]);
+        }
+    }
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EsnUab\\Libro\\Model\\Socio',
+            'csrf_protection'   => true,
+            'action_type' => 'crear',
+        ));
+    }
+    public function getName()
+    {
+        return "socio";
+    }
+
+    private function getIdiomaValues()
+    {
+        return SocioTableMap::getValueSet(SocioTableMap::COL_IDIOMA);
+    }
+}
